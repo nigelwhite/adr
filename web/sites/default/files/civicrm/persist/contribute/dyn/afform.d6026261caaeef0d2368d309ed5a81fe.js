9148a16fcda7aafc39b2c1c87b0d5ac453ec6c1a
nigelwhite <webdev@permaculture.org.uk>
2024-12-08 16:09:11 +0000

    css for home page

--- web/sites/default/files/civicrm/persist/contribute/dyn/afform.d6026261caaeef0d2368d309ed5a81fe.js
+++
@@ -1,9 +0,0 @@
-(function(angular, $, _) {
-  angular.module('afformAddContact', CRM.angRequires('afformAddContact'));
-  angular.module('afformAddContact').directive('afformAddContact', function(afCoreDirective) {
-    return afCoreDirective("afformAddContact", {"title":"Add contact","redirect":"\/node\/1","name":"afformAddContact"}, {
-      templateUrl: "~\/afformAddContact\/afformAddContact.aff.html"
-    });
-  });
-})(angular, CRM.$, CRM._);
-
